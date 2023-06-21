83cf0aeb51e5a06faf82bc6be4b70bd846fcf38e
arxivj <134363495+arxivj@users.noreply.github.com>
2023-06-21 17:43:46 +0900

    [Bronze II] Title: 16진수, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bufferedWriter.write(String.valueOf(Integer.parseInt(bufferedReader.readLine(),16)));
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
